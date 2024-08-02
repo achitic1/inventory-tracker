9b0103a45248448dc8ec396e4408e50b4bf0b6c4
achitic1 <chitianthony52@gmail.com>
2024-08-02 18:02:47 -0400

    Add styling to UI and a remove button to remove items

--- inventory-management/app/page.js
+++ inventory-management/app/page.js
@@ -1,9 +1,32 @@
-"use client"
-import Image from "next/image";
-import { useState, useEffect} from "react"
-import { firestore } from "@/firebase";
-import { Box, Modal, Typography, Stack, TextField, Button } from "@mui/material"; 
-import { doc, getDocs, query, collection, getDoc, setDoc, deleteDoc } from "firebase/firestore";
+'use client'
+
+import { useState, useEffect } from 'react'
+import { Box, Stack, Typography, Button, Modal, TextField } from '@mui/material'
+import { firestore } from '@/firebase'
+import {
+  collection,
+  doc,
+  getDocs,
+  query,
+  setDoc,
+  deleteDoc,
+  getDoc,
+} from 'firebase/firestore'
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'white',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 3,
+}
 
 export default function Home() {
   // Initializing the state variables
@@ -20,8 +43,7 @@ export default function Home() {
     docs.forEach((doc) => {
       inventoryList.push({ name: doc.id, ...doc.data() });
     });
-    setInventory[inventoryList];
-    console.log("Fetched Inventory: ", inventoryList);
+    setInventory(inventoryList);
   }
 
   const addItem = async(item) => {
